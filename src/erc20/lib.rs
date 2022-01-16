88f6cf3e6a52f040a575a29cccef424dd44e66dd
Hugo Lageneste <hugo.lageneste@pm.me>
2022-01-16 11:58:11 +0100

    Initial commit

--- src/erc20/lib.rs
+++ src/erc20/lib.rs
@@ -1,3 +1,17 @@
+use ic_cdk_macros::*;
+use ic_cdk::export::candid::{types::number::Nat};
+use ic_kit::{Principal};
+use std::collections::HashMap;
+
+type Balances = HashMap<Principal, Nat>
+
+let max_supply = 5000000
+
+#[init]
+fn init() {
+
+}
+
 #[ic_cdk_macros::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
