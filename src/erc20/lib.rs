f10992e5af1632c3446fb14a9e46fdc7c6f259b1
Hugo Lageneste <hugo.lageneste@pm.me>
2022-01-16 17:44:45 +0100

    Add getters for name, symbol, decimals

--- src/erc20/lib.rs
+++ src/erc20/lib.rs
@@ -7,15 +7,50 @@ use std::collections::HashMap;
 
 type Balances = HashMap<Principal, Nat>;
 
+struct Token {
+  name: str,
+  symbol: str,
+  decimals: u8,
+  total_supply: Nat,
+}
+
 #[init]
 fn init() {
     // Initialize metadata for the ERC20 token
-    let principal_id = Principal::from_text("kp36f-wbon5-rq45k-vo3r3-fjwqq-jxufl-znnnn-5k35g-7xugj-ggthi-mqe").unwrap();
-    let total_supply = ic::get_mut::<Nat>();
-    *total_supply = Nat::from(5000000);
-    let balances = ic::get_mut::<Balances>();
+    let token = ic::get_mut::<Token>();
+    token.name = "Hugo";
+    token.symbol = "HGL";
+    token.decimals = 3;
+    token.total_supply = Nat::from(5000000);
 
-    balances.insert(principal_id, total_supply.clone());
+    let principal_id = Principal::from_text("kp36f-wbon5-rq45k-vo3r3-fjwqq-jxufl-znnnn-5k35g-7xugj-ggthi-mqe").unwrap();
+    let balances = ic::get_mut::<Balances>();
+    balances.insert(principal_id, token.total_supply);
+}
+
+#[query]
+fn name() -> str {
+    let name = ic::get::<Token>();
+    token.name.clone()
+}
+
+#[query]
+fn symbol() -> str {
+    let symbol = ic::get::<Token>();
+    token.symbol.clone()
+}
+
+#[query]
+fn decimals() -> u8 {
+    let decimals = ic::get::<Token>();
+    token.decimals.clone()
+}
+
+#[update(name = "totalSupply")]
+#[query]
+fn total_supply() -> Nat {
+    let token = ic::get::<Token>();
+    token.total_supply.clone()
 }
 
 #[update(name = "balanceOf")]
