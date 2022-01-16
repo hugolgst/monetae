c145e48e7d4f8a4c3275c106a58d50ce03f305e9
Hugo Lageneste <hugo.lageneste@pm.me>
2022-01-16 19:44:22 +0100

    Create transfer from function

--- src/erc20/lib.rs
+++ src/erc20/lib.rs
@@ -4,45 +4,57 @@ use ic_cdk_macros::*;
 use ic_cdk::export::candid::{types::number::Nat};
 use ic_kit::{ic, Principal};
 use std::collections::HashMap;
+use std::string::String;
 
 type Balances = HashMap<Principal, Nat>;
 
 struct Token {
-  name: str,
-  symbol: str,
-  decimals: u8,
-  total_supply: Nat,
+    name: String,
+    symbol: String,
+    decimals: u8,
+    total_supply: Nat,
+}
+
+impl Default for Token {
+    fn default() -> Self {
+        Self {
+            name: String::from(""),
+            symbol: String::from(""),
+            decimals: 0u8,
+            total_supply: Nat::from(0),
+        }
+    }
 }
 
 #[init]
 fn init() {
     // Initialize metadata for the ERC20 token
     let token = ic::get_mut::<Token>();
-    token.name = "Hugo";
-    token.symbol = "HGL";
+    token.name = String::from("Hugo");
+    token.symbol = String::from("HGL");
     token.decimals = 3;
     token.total_supply = Nat::from(5000000);
 
     let principal_id = Principal::from_text("kp36f-wbon5-rq45k-vo3r3-fjwqq-jxufl-znnnn-5k35g-7xugj-ggthi-mqe").unwrap();
     let balances = ic::get_mut::<Balances>();
-    balances.insert(principal_id, token.total_supply);
+    balances.insert(principal_id, token.total_supply.clone());
 }
 
 #[query]
-fn name() -> str {
-    let name = ic::get::<Token>();
+fn name() -> String {
+    let token = ic::get::<Token>();
     token.name.clone()
 }
 
 #[query]
-fn symbol() -> str {
-    let symbol = ic::get::<Token>();
+fn symbol() -> String {
+    let token = ic::get::<Token>();
     token.symbol.clone()
 }
 
 #[query]
 fn decimals() -> u8 {
-    let decimals = ic::get::<Token>();
+    let token = ic::get::<Token>();
     token.decimals.clone()
 }
 
@@ -55,10 +67,25 @@ fn total_supply() -> Nat {
 
 #[update(name = "balanceOf")]
 #[query]
-fn balance_of(id: Principal) -> Nat {
+fn balance_of(owner: Principal) -> Nat {
     let balances = ic::get::<Balances>();
-    match balances.get(&id) {
+    match balances.get(&owner) {
         Some(balance) => balance.clone(),
         None => Nat::from(0),
     }
+}
+
+#[update(name = "transferFrom")]
+#[update]
+fn transfer_from(from: Principal, to: Principal, value: Nat) -> bool {
+    let balance_from = balance_of(from);
+    if balance_from < value || value == 0 {
+        return false;
+    }
+
+    let balances = ic::get_mut::<Balances>();
+    balances.insert(from, balance_from - value.clone());
+    balances.insert(to, balance_of(to) + value.clone());
+
+    return true;
 }
