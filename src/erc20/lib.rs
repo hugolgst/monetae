8a578696d8b33aedb9dbe4682199b7fd0c5ff039
Hugo Lageneste <hugo.lageneste@pm.me>
2022-01-24 17:34:04 +0100

    Separate types in a new file

--- src/erc20/lib.rs
+++ src/erc20/lib.rs
@@ -1,74 +1,55 @@
 // ERC-20 standard: https://eips.ethereum.org/EIPS/eip-20
 
+mod types;
+
 use ic_cdk_macros::*;
 use ic_cdk::export::candid::{types::number::Nat};
 use ic_kit::{ic, Principal};
-use std::collections::HashMap;
 use std::string::String;
-
-type Balances = HashMap<Principal, Nat>;
-
-struct Token {
-    name: String,
-    symbol: String,
-    decimals: u8,
-    total_supply: Nat,
-}
-
-impl Default for Token {
-    fn default() -> Self {
-        Self {
-            name: String::from(""),
-            symbol: String::from(""),
-            decimals: 0u8,
-            total_supply: Nat::from(0),
-        }
-    }
-}
 
 #[init]
 fn init() {
     // Initialize metadata for the ERC20 token
-    let token = ic::get_mut::<Token>();
+    let token = ic::get_mut::<types::Token>();
     token.name = String::from("Hugo");
     token.symbol = String::from("HGL");
     token.decimals = 3;
     token.total_supply = Nat::from(5000000);
 
     let principal_id = Principal::from_text("kp36f-wbon5-rq45k-vo3r3-fjwqq-jxufl-znnnn-5k35g-7xugj-ggthi-mqe").unwrap();
-    let balances = ic::get_mut::<Balances>();
+    let balances = ic::get_mut::<types::Balances>();
     balances.insert(principal_id, token.total_supply.clone());
 }
 
 #[query]
 fn name() -> String {
-    let token = ic::get::<Token>();
+    let token = ic::get::<types::Token>();
     token.name.clone()
 }
 
 #[query]
 fn symbol() -> String {
-    let token = ic::get::<Token>();
+    let token = ic::get::<types::Token>();
     token.symbol.clone()
 }
 
 #[query]
 fn decimals() -> u8 {
-    let token = ic::get::<Token>();
+    let token = ic::get::<types::Token>();
     token.decimals.clone()
 }
 
 #[update(name = "totalSupply")]
 #[query]
 fn total_supply() -> Nat {
-    let token = ic::get::<Token>();
+    let token = ic::get::<types::Token>();
     token.total_supply.clone()
 }
 
 #[update(name = "balanceOf")]
 #[query]
 fn balance_of(owner: Principal) -> Nat {
-    let balances = ic::get::<Balances>();
+    let balances = ic::get::<types::Balances>();
     match balances.get(&owner) {
         Some(balance) => balance.clone(),
         None => Nat::from(0),
@@ -83,7 +64,7 @@ fn transfer_from(from: Principal, to: Principal, value: Nat) -> bool {
         return false;
     }
 
-    let balances = ic::get_mut::<Balances>();
+    let balances = ic::get_mut::<types::Balances>();
     balances.insert(from, balance_from - value.clone());
     balances.insert(to, balance_of(to) + value.clone());
 
---
+++ src/erc20/types.rs
@@ -0,0 +1,23 @@
+use ic_kit::{Principal};
+use ic_cdk::export::candid::{types::number::Nat};
+use std::collections::HashMap;
+
+pub type Balances = HashMap<Principal, Nat>;
+
+pub struct Token {
+    pub name: String,
+    pub symbol: String,
+    pub decimals: u8,
+    pub total_supply: Nat,
+}
+
+impl Default for Token {
+    fn default() -> Self {
+        Self {
+            name: String::from(""),
+            symbol: String::from(""),
+            decimals: 0u8,
+            total_supply: Nat::from(0),
+        }
+    }
+}
