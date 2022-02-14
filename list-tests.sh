d1ded18e8a3b58dcaa311d3c7ce74b3072b263e5
Hugo Lageneste <hugo.lageneste@pm.me>
2022-02-14 23:00:57 +0100

    Add a parameter to the test-lists.sh

--- list-tests.sh
+++ list-tests.sh
@@ -1,1 +1,1 @@
-cargo test -- --list | awk '/: test/{print substr($1, 1, length($1)-1)}' | jq  --raw-input .  | jq --slurp .
+cargo test -- --list | awk '/: test/{print substr($1, 1, length($1)-1)}' | grep $1 | jq  --raw-input .  | jq --slurp .
