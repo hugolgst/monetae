28602b48e5866bc867fdfaa1c9c8fff18fc19eda
Hugo Lageneste <hugo.lageneste@dfinity.org>
2022-02-24 19:13:46 +0100

    Hash the wasm file in build

--- .github/workflows/tools/build.sh
+++ .github/workflows/tools/build.sh
@@ -2,13 +2,20 @@
 
 sh -ci "$(curl -fsSL https://sdk.dfinity.org/install.sh)"
 
+dfx start --background
+
 if [ "$1" == "frontend" -o $# -eq 0 ]; then
   cd "$GITHUB_WORKSPACE/src/frontend" && npm install
+  dfx canister create frontend
+  dfx build frontend
 fi
 
 if [ "$1" == "contract" -o $# -eq 0 ]; then
   cargo install ic-cdk-optimizer
   rustup target add wasm32-unknown-unknown
+  dfx canister create contract
+  dfx build contract
 fi
 
-dfx start --background
+echo "\nModule hash:"
+sha512sum target/wasm32-unknown-unknown/release/contract.wasm | awk '{print $1}'
